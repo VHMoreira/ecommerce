ac0fb3e2c9e3cde8131fa259cc873a95ebf6a76a
Vitor Henrique <vhsouzaeng@gmail.com>
2020-05-21 17:25:49 -0300

    ItemProducts

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/components/product_item.dart';
 import '../models/Products.dart';
 import '../data/dummy_data.dart';
 
@@ -15,7 +16,7 @@ class ProductOverviewScreen extends StatelessWidget {
       body: GridView.builder(
         padding: const EdgeInsets.all(10),
         itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
+        itemBuilder: (ctx, i) => ProductItem(loadedProducts[i]),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3/2,
