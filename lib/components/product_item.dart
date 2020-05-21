b442ca1b79912a946c253ea9b7fc7d4cff8a9c6e
Vitor Henrique <vhsouzaeng@gmail.com>
2020-05-21 18:17:07 -0300

    Navegacao nomeada

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop/models/Products.dart';
+import 'package:shop/utils/app_routes.dart';
+import 'package:shop/views/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
 
@@ -9,31 +11,41 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        product.imageUrl, 
-        fit: BoxFit.cover
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black87,
-        leading: IconButton(
-          color: Theme.of(context).accentColor,
-          icon: Icon(Icons.favorite),
-          onPressed: (){
-
+    return ClipRRect(
+        child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              AppRoutes.PRODUCT_DETAIL, 
+              arguments: product
+            );
           },
-        ),
-        title: Text(
-          product.title,
-          textAlign: TextAlign.center,
-        ),
-        trailing: IconButton(
-          color: Theme.of(context).accentColor,
-          icon: Icon(Icons.shopping_cart),
-          onPressed: () {
+          child: Image.network(
+              product.imageUrl, 
+              fit: BoxFit.cover
+            ),
+          ),
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(
+              color: Theme.of(context).accentColor,
+              icon: Icon(Icons.favorite),
+              onPressed: (){
 
-          },
-        ),
+              },
+            ),
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+              color: Theme.of(context).accentColor,
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {
+
+              },
+            ),
+          ),
       ),
     );
   }
