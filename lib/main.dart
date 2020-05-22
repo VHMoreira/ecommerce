6c5e2dfcf0ca5da12b4d39c7a7f9d08420b2523f
Vitor Henrique <vhsouzaeng@gmail.com>
2020-05-22 01:46:01 -0300

    Multiproviders para ter varios providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/providers/cart.dart';
 import 'package:shop/providers/products.dart';
 import 'package:shop/utils/app_routes.dart';
 import 'package:shop/views/product_detail_screen.dart';
@@ -10,19 +11,23 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'Minha Loja',
         theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: 'Lato'
-        ),
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato'),
         home: ProductOverviewScreen(),
-        routes: {
-          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen()
-        },
+        routes: {AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen()},
       ),
     );
   }
